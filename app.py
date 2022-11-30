551edb209deefa7243966113fac4c175ea4193e3
kamal0801 <119513152+kamal0801@users.noreply.github.com>
2022-11-30 20:28:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 import streamlit as st
 
 st.write("""
-# Finding maximum of the 3 input parameters
+# Finding multiplication of the 2 input parameters
 """)
 # Get Input
 st.header('User Input Parameters')
 def user_input_features():
   number1=st.number_input("Input 1st number here :")
   number2=st.number_input("Input 2nd number here :")
-  number3=st.number_input("Input 3rd number here :")
-  data={'number1':number1,'number2':number2,'number3':number3}
+  
+  data={'number1':number1,'number2':number2}
  
-  maxnum = max(number1,number2,number3)
+  multiply = number1 * number2
 
-  return maxnum
+  return multiply
 
 df = user_input_features()
-st.write('Max number:')
+st.write('Multiplication answer :')
 st.write(df)
 
 
